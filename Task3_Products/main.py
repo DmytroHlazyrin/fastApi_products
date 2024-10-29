78576e3e9c59f9c9d4a41fc7af70f0a23b24ae03
DmytroHlazyrin <dmytro.hlazyrin@gmail.com>
2024-10-29 17:24:19 +0200

    Deleted redundant creating metadata

--- Task3_Products/main.py
+++ Task3_Products/main.py
@@ -4,8 +4,6 @@ from Task3_Products import models
 from Task3_Products.database import engine
 from Task3_Products.routers import categories, products
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="Shop")
 
 app.include_router(categories.router, tags=["Categories"])
